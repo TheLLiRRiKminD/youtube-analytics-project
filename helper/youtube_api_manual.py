98ab69759b6af11b7c0b01dda4ba2fb7d6a9d1f8
LLiRRiK <kill2002@mail.ru>
2023-08-14 20:21:58 +0300

    init project and add requirements

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,6 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+# api_key = 'AIzaSyDwH2OicDGCBavZH-bArQhGGzxhBlFCMCc'
 api_key: str = os.getenv('YT_API_KEY')
 
 # создать специальный объект для работы с API
