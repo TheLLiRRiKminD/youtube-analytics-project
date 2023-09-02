867f0e50ee8a169c595f55660b8c4731122f118e
LLiRRiK <kill2002@mail.ru>
2023-09-02 13:16:12 +0300

    improvements homework_1

--- src/channel.py
+++ src/channel.py
@@ -1,17 +1,21 @@
+import os
 from googleapiclient.discovery import build
-import isodate, json
-
-api_key = 'AIzaSyDwH2OicDGCBavZH-bArQhGGzxhBlFCMCc'
-youtube = build('youtube', 'v3', developerKey=api_key)
-
-
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+import json
+from dotenv import load_dotenv
 
 
 class Channel:
     """Класс для ютуб-канала"""
+
+    load_dotenv()
+
+    api_key = os.getenv('YT_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    @staticmethod
+    def printj(dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -19,5 +23,5 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        printj(channel)
+        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        Channel.printj(channel)
