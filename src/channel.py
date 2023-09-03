5528ed6dce7f9684294c1c6ed26c903833ba620f
LLiRRiK <kill2002@mail.ru>
2023-09-03 18:55:52 +0300

    stable homework_3 version 1

--- src/channel.py
+++ src/channel.py
@@ -2,10 +2,11 @@ import os, json
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 
+load_dotenv()
+
+
 class Channel:
     """Класс для ютуб-канала"""
-
-    load_dotenv()
 
     api_key = os.getenv('YT_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
@@ -20,6 +21,28 @@ class Channel:
         self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
         self.video_count = channel['items'][0]['statistics']['videoCount']
         self.view_count = channel['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        return f'{self.title}({self.url})'
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __it__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
     @staticmethod
     def printj(dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
@@ -27,8 +50,8 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        Channel.printj(channel)
+        channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.printj(channel)
 
     @property
     def channel_id(self):
