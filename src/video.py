112248deca144700bbf9be9bb7d887e09c77c282
LLiRRiK <kill2002@mail.ru>
2023-09-06 21:51:56 +0300

    homework_6 stable version 1

--- src/video.py
+++ src/video.py
@@ -10,13 +10,20 @@ class Video:
 
     def __init__(self, video_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.__video_id = video_id
-        self.video_url = f"https://www.youtube.com/watch?v={video_id}"
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id).execute()
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.__video_id = video_id
+            self.video_url = f"https://www.youtube.com/watch?v={video_id}"
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id).execute()
+            self.video_title = video_response['items'][0]['snippet']['title']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.__video_id = None
+            self.video_url = None
+            self.video_title = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f'{self.video_title}'
